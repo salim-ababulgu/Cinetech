4153f6e4d0101570f70f76d706e72d5be0b46416
Salim.ababulgu <salim.ababulgu@laplateforme.io>
2024-04-22 15:22:37 +0200

    mis a jour du projet

--- navbar.js
+++ navbar.js
@@ -1,11 +1,12 @@
 // Récupère le conteneur du Navbar
 const navbarContainer = document.getElementById('navbar');
 
-// Charge le contenu de navbar.html
-fetch('./navbar.html')
+// Charge le contenu de navbar.html depuis la branche navbar
+fetch('exemple-navbar.html')
   .then(response => response.text())
   .then(data => {
     // Injecte le contenu dans le conteneur du Navbar
     navbarContainer.innerHTML = data;
   })
   .catch(error => console.error('Une erreur s\'est produite : ', error));
+
