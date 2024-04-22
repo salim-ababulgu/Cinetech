22aff14665bf8cd4f9537f9601e35e2bde6f26c2
Salim.ababulgu <salim.ababulgu@laplateforme.io>
2024-04-22 14:45:46 +0200

    Mise a jour de navbar.js

--- navbar.js
+++ navbar.js
@@ -1,13 +1,11 @@
-// Récupérer le chemin de la page actuelle //
-const currentPagePath = window.location.pathname;
+// Récupère le conteneur du Navbar
+const navbarContainer = document.getElementById('navbar');
 
-// Charger le Navbar sur toutes les pages sauf index.html
-if (currentPagePath !== "/navbar.html") {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            // Insérer le contenu du Navbar dans la balise <div id="navbar">
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Une erreur s\'est produite :', error));
-}
+// Charge le contenu de navbar.html depuis la branche navbar
+fetch('./navbar/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    // Injecte le contenu dans le conteneur du Navbar
+    navbarContainer.innerHTML = data;
+  })
+  .catch(error => console.error('Une erreur s\'est produite : ', error));
